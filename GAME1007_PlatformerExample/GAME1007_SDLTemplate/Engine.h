a1b8dc08e7b35749e51821a025d853ae963681b5
Ken C <zhikang.chen@georgebrown.ca>
2021-01-25 21:37:55 -0500

    Added Example

---
+++ GAME1007_PlatformerExample/GAME1007_SDLTemplate/Engine.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include "SDL.h"
+#include <iostream>
+#define FPS 60
+#define WIDTH 1024
+#define HEIGHT 768
+using namespace std;
+
+class Engine
+{
+private: // private properties.
+	bool m_running = false;
+	Uint32 m_start, m_end, m_delta, m_fps;
+	const Uint8* m_keystates;
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+
+private: // private method prototypes.
+	int Init(const char* title, int xPos, int yPos, int width, int height, int flags);
+	void Clean();
+	void Wake();
+	void HandleEvents();
+	void Update();
+	void Render();
+	void Sleep();
+
+public: // public method prototypes.
+	int Run();
+};
+
+#endif
+
+// Reminder: you can ONLY have declarations in headers, not logical code
