2e5824d38796d2bc4e2448a83535305e8fb2055c
Ken C <zhikang.chen@georgebrown.ca>
2021-04-10 19:24:23 -0400

    Update Slime.cpp

--- GAME1014_Project/src/Slime.cpp
+++ GAME1014_Project/src/Slime.cpp
@@ -14,6 +14,7 @@ void Slime::Move()
 	m_jumpTimer++;
 	if(m_jumpTimer >= 120)
 	{
+		m_grounded = false;
 		SoundManager::PlaySound("bounce", 0, 3);
 		m_jumpTimer = 0;
 		if(m_facingLeft)
@@ -22,6 +23,16 @@ void Slime::Move()
 			m_accelX = -30.0f;
 		m_accelY = -JUMPFORCE; // SetAccelY(-JUMPFORCE);
 	}
+	else if(m_jumpTimer >= 110)
+	{
+		SetColour(180, 180, 255);
+	}
+	else if(m_grounded)
+	{
+		SetColour(255, 255, 255);
+	}
+
+
 
 	//m_accelY = JUMPFORCE;
 }
